796ec7f6c57bf1db0149a0830845ac98a5b6bb60
Francisco Flores <francisco.giavay@gmail.com>
2025-01-29 14:06:19 -0300

    impl: scanner: identifiers

--- scanner.c
+++ scanner.c
@@ -8,11 +8,15 @@ static Token makeToken(TokenType tokenType);
 static Token errorToken(char* message);
 static Token string();
 static Token number();
+static Token identifier();
+static TokenType identifierType();
+static TokenType checkKeyWord(int start, int lenght, const char* rest, TokenType type);
 static void skipWhiteSpace();
 static char advance();
 static char peek();
 static char peekNext();
 static bool isDigit(char c);
+static bool isAlpha(char c);
 static bool match(char c);
 static bool isAtEnd();
 
@@ -39,6 +43,10 @@ Token scanToken() {
     }
 
     char c = advance();
+
+    if (isAlpha(c)) {
+        return identifier();
+    }
     if (isDigit(c)) {
         return number();
     }
@@ -118,6 +126,55 @@ static Token number() {
     return makeToken(TOKEN_NUMBER);
 }
 
+static Token identifier() {
+    while (isAlpha(peek()) || isDigit(peek())) {
+        advance();
+    }
+    return makeToken(identifierType());
+}
+
+static TokenType identifierType() {
+    switch (scanner.start[0]) {
+        case 'a': return checkKeyWord(1, 2, "nd", TOKEN_AND);
+        case 'c': return checkKeyWord(1, 4, "lass", TOKEN_CLASS);
+        case 'e': return checkKeyWord(1, 3, "lse", TOKEN_ELSE);
+        case 'f': 
+                  if (scanner.current - scanner.start > 1) {
+                      switch (scanner.start[1]) {
+                          case 'a': return checkKeyWord(2, 3, "lse", TOKEN_FALSE);
+                          case 'o': return checkKeyWord(2, 1, "r", TOKEN_FOR);
+                          case 'u': return checkKeyWord(2, 1, "n", TOKEN_FUN);
+                      }
+                  }
+                  break;
+        case 'i': return checkKeyWord(1, 1, "f", TOKEN_IF);
+        case 'n': return checkKeyWord(1, 2, "il", TOKEN_NIL);
+        case 'o': return checkKeyWord(1, 1, "r", TOKEN_OR);
+        case 'p': return checkKeyWord(1, 4, "rint", TOKEN_PRINT);
+        case 'r': return checkKeyWord(1, 5, "eturn", TOKEN_RETURN);
+        case 's': return checkKeyWord(1, 4, "uper", TOKEN_SUPER);
+        case 't': 
+                  if (scanner.current - scanner.start > 1) {
+                      switch (scanner.start[1]) {
+                          case 'h': return checkKeyWord(2, 2, "is", TOKEN_THIS);
+                          case 'r': return checkKeyWord(2, 2, "ue", TOKEN_TRUE);
+                      }
+                  }
+                  break;
+        case 'v': return checkKeyWord(1, 2, "ar", TOKEN_VAR);
+        case 'w': return checkKeyWord(1, 4, "hile", TOKEN_WHILE);
+  }
+    return TOKEN_IDENTIFIER;
+}
+
+static TokenType checkKeyWord(int start, int lenght, const char* rest, TokenType type) {
+    if (scanner.current - scanner.start == start + lenght &&
+        memcmp(scanner.start + start, rest, lenght) == 0) {
+        return type;
+    }
+    return TOKEN_IDENTIFIER;
+}
+
 static void skipWhiteSpace() {
     for (;;) {
         char c = peek();
@@ -171,6 +228,12 @@ static bool match(char c) {
     }
     scanner.current++;
     return true;
+}
+
+static bool isAlpha(char c) {
+    return c >= 'a' && c <= 'z' ||
+           c >= 'A' && c <= 'Z' ||
+           c == '_';
 }
 
 static bool isDigit(char c) {
